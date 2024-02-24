570c912c0fe8c673082e210cf8491b183d93bb6c
Muhannad Bahurmoz <moe.hurmoz@gmail.com>
2024-02-25 02:06:14 +0300

    API Endpoint & loader function modified, & updated javascript version

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,5 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
-    return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
+// import axios from "axios"; // If you run the import, an error will appear in the browser and the app will stop
 const countryElement = document.getElementById("Country"),
   cityElement = document.getElementById("City"),
   methodElement = document.getElementById("Method");
@@ -78,85 +39,88 @@ function continueClicked() {
   localStorage.setItem("Method", method);
 }
 // LOADER FUNCTIN:
-function loader(elementID) {
-  document.getElementById(`${elementID}`).innerHTML = `
-  <span class="loader"></span>
-  `;
+function loader(elementsId, state, message = "Something's Wrong") {
+  if (state === "on") {
+    for (let element of elementsId) {
+      document.getElementById(`${element}`).innerHTML = `
+      <span class="loader"></span>
+      `;
+    }
+  } else {
+    for (let element of elementsId) {
+      document.getElementById(`${element}`).innerHTML = `
+      <span style="color: red">${message}</span>
+      `;
+    }
+  }
 }
 // GET DATA AND HANDLING:
-function getDataFromAPI(country = "SA", city = "Mecca", method = 4) {
-  // HANDLING WITH LOADER:
-  loader("Hijri");
-  loader("Gregorian");
-  loader("f");
-  loader("s");
-  loader("d");
-  loader("a");
-  loader("m");
-  loader("i");
-  loader("month-table");
-  function callAPI() {
-    return __awaiter(this, void 0, void 0, function* () {
-      try {
-        // GET DATA:
-        const year = new Date().getFullYear(),
-          month = new Date().getMonth() + 1,
-          day = new Date().getDate() - 1,
-          endpoint = `https://api.aladhan.com/v11/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=${method}`;
-        // API calling:
-        const response = yield axios_1.default.get(endpoint), // Todo, Type Annotation
-          res = response.data.data;
-        // HANDLING WITH DATE:
-        const gregorian = res[day].date.gregorian,
-          gFullDateNo = gregorian.date,
-          gYear = gregorian.year,
-          gMonth = gregorian.month.en,
-          gDay = gregorian.weekday.en,
-          gDayNo = gregorian.day;
-        const hijri = res[day].date.hijri,
-          hFullDateNo = hijri.date,
-          hYear = hijri.year,
-          hMonth = hijri.month.en,
-          hDay = hijri.weekday.en,
-          hDayNo = hijri.day;
-        document.getElementById("Gregorian").innerHTML = `
+async function getDataFromAPI(country = "SA", city = "Mecca", method = 4) {
+  try {
+    // RUN THE LOADER:
+    loader(
+      ["Hijri", "Gregorian", "f", "s", "d", "a", "m", "i", "month-table"],
+      "on"
+    );
+    // GET DATA:
+    const year = new Date().getFullYear(),
+      month = new Date().getMonth() + 1,
+      day = new Date().getDate() - 1,
+      endpoint = `https://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=${method}`;
+    // API calling:
+    const response = await axios.get(endpoint), // Todo, Type Annotation
+      res = response.data.data;
+    // HANDLING WITH DATE:
+    const gregorian = res[day].date.gregorian,
+      gFullDateNo = gregorian.date,
+      gYear = gregorian.year,
+      gMonth = gregorian.month.en,
+      gDay = gregorian.weekday.en,
+      gDayNo = gregorian.day;
+    const hijri = res[day].date.hijri,
+      hFullDateNo = hijri.date,
+      hYear = hijri.year,
+      hMonth = hijri.month.en,
+      hDay = hijri.weekday.en,
+      hDayNo = hijri.day;
+    document.getElementById("Gregorian").innerHTML = `
       <h2 class="title-font">Gregorian</h2>
       <h5 class="secondary-font mt-4">${gFullDateNo}</h5>
       <h5 class="secondary-font mt-1">${gDay}  ${gDayNo} ${gMonth}  ${gYear}</h5>
       `;
-        document.getElementById("Hijri").innerHTML = `
+    document.getElementById("Hijri").innerHTML = `
       <h2 class="title-font">Hijri</h2>
       <h5 class="secondary-font mt-4">${hFullDateNo}</h5>
       <h5 class="secondary-font mt-1">${hDay}  ${hDayNo} ${hMonth}  ${hYear}</h5>
       `;
-        // HANDLING WITH TODAY'S TIMINGS:
-        const timings = res[day].timings,
-          fajr = to12Format(timings.Fajr.slice(0, 5)),
-          sunrise = to12Format(timings.Sunrise.slice(0, 5)),
-          dhuhr = to12Format(timings.Dhuhr.slice(0, 5)),
-          asr = to12Format(timings.Asr.slice(0, 5)),
-          maghrib = to12Format(timings.Maghrib.slice(0, 5)),
-          isha = to12Format(timings.Isha.slice(0, 5));
-        document.getElementById("f").innerHTML = `
+    // HANDLING WITH TODAY'S TIMINGS:
+    const timings = res[day].timings,
+      fajr = to12Format(timings.Fajr.slice(0, 5)),
+      sunrise = to12Format(timings.Sunrise.slice(0, 5)),
+      dhuhr = to12Format(timings.Dhuhr.slice(0, 5)),
+      asr = to12Format(timings.Asr.slice(0, 5)),
+      maghrib = to12Format(timings.Maghrib.slice(0, 5)),
+      isha = to12Format(timings.Isha.slice(0, 5));
+    document.getElementById("f").innerHTML = `
       <h4 class="secondary-font mb-0">${fajr}</h4>
       `;
-        document.getElementById("s").innerHTML = `
+    document.getElementById("s").innerHTML = `
       <h4 class="secondary-font mb-0">${sunrise}</h4>
       `;
-        document.getElementById("d").innerHTML = `
+    document.getElementById("d").innerHTML = `
       <h4 class="secondary-font mb-0">${dhuhr}</h4>
       `;
-        document.getElementById("a").innerHTML = `
+    document.getElementById("a").innerHTML = `
       <h4 class="secondary-font mb-0">${asr}</h4>
       `;
-        document.getElementById("m").innerHTML = `
+    document.getElementById("m").innerHTML = `
       <h4 class="secondary-font mb-0">${maghrib}</h4>
       `;
-        document.getElementById("i").innerHTML = `
+    document.getElementById("i").innerHTML = `
       <h4 class="secondary-font mb-0">${isha}</h4>
       `;
-        // HANDLING WITH DAYS OF MONTH TIMINGS:
-        document.getElementById("month-table").innerHTML = `
+    // HANDLING WITH DAYS OF MONTH TIMINGS:
+    document.getElementById("month-table").innerHTML = `
         <thead class="align">
             <tr>
                 <th scope="col"></th>
@@ -170,35 +134,36 @@ function getDataFromAPI(country = "SA", city = "Mecca", method = 4) {
         </thead>
         <tbody id="tbody" class="align"> </tbody>
       `;
-        const tableBody = document.getElementById("tbody");
-        tableBody.innerHTML = "";
-        res.forEach((obj) => {
-          const Day = obj.date.gregorian.weekday.en.slice(0, 3),
-            date = obj.date.readable.slice(0, 6),
-            Fajr = to12Format(obj.timings.Fajr.slice(0, 5)),
-            Sunrise = to12Format(obj.timings.Sunrise.slice(0, 5)),
-            Dhuhr = to12Format(obj.timings.Dhuhr.slice(0, 5)),
-            Asr = to12Format(obj.timings.Asr.slice(0, 5)),
-            Maghrib = to12Format(obj.timings.Maghrib.slice(0, 5)),
-            Isha = to12Format(obj.timings.Isha.slice(0, 5));
-          const todayMark =
-            date.match(/\d+/)[0] === `${new Date().getDate()}`
-              ? "today-mark"
-              : "";
-          if (screen.width >= 768) {
-            tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
-          } else {
-            tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} <br/> ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
-          }
-        });
-      } catch (err) {
-        // Todo, 'err' is of type 'unknown'
-        alert(err.message);
-        throw new Error(`${err.code},\n${err.message}`);
+    const tableBody = document.getElementById("tbody");
+    tableBody.innerHTML = "";
+    res.forEach((obj) => {
+      const Day = obj.date.gregorian.weekday.en.slice(0, 3),
+        date = obj.date.readable.slice(0, 6),
+        Fajr = to12Format(obj.timings.Fajr.slice(0, 5)),
+        Sunrise = to12Format(obj.timings.Sunrise.slice(0, 5)),
+        Dhuhr = to12Format(obj.timings.Dhuhr.slice(0, 5)),
+        Asr = to12Format(obj.timings.Asr.slice(0, 5)),
+        Maghrib = to12Format(obj.timings.Maghrib.slice(0, 5)),
+        Isha = to12Format(obj.timings.Isha.slice(0, 5));
+      const todayMark =
+        date.match(/\d+/)[0] === `${new Date().getDate()}` ? "today-mark" : "";
+      if (screen.width >= 768) {
+        tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
+      } else {
+        tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} <br/> ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
       }
     });
+  } catch (err) {
+    // Todo, Type Annotation
+    alert(err.message);
+    // STOP THE LOADER:
+    loader(
+      ["Hijri", "Gregorian", "f", "s", "d", "a", "m", "i", "month-table"],
+      "off",
+      err.message
+    );
+    throw new Error(`${err.code},\n${err.message}`);
   }
-  callAPI();
 }
 // Function to convert the time from 24 to 12 format:
 function to12Format(numberString) {
--- ts/scripts.ts
+++ ts/scripts.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios"; // If you run the import, an error will appear in the browser and the app will stop
 
 const countryElement = document.getElementById("Country") as HTMLSelectElement,
   cityElement = document.getElementById("City") as HTMLSelectElement,
@@ -44,164 +44,170 @@ function continueClicked(): void {
 }
 
 // LOADER FUNCTIN:
-function loader(elementID: string): void {
-  document.getElementById(`${elementID}`)!.innerHTML = `
-  <span class="loader"></span>
-  `;
+function loader(
+  elementsId: string[],
+  state: "on" | "off",
+  message: string = "Something's Wrong"
+): void {
+  if (state === "on") {
+    for (let element of elementsId) {
+      document.getElementById(`${element}`)!.innerHTML = `
+      <span class="loader"></span>
+      `;
+    }
+  } else {
+    for (let element of elementsId) {
+      document.getElementById(`${element}`)!.innerHTML = `
+      <span style="color: red">${message}</span>
+      `;
+    }
+  }
 }
 
 // GET DATA AND HANDLING:
-function getDataFromAPI(
+async function getDataFromAPI(
   country: string = "SA",
   city: string = "Mecca",
   method: number = 4
 ) {
-  // HANDLING WITH LOADER:
-  loader("Hijri");
-  loader("Gregorian");
+  try {
+    // RUN THE LOADER:
+    loader(
+      ["Hijri", "Gregorian", "f", "s", "d", "a", "m", "i", "month-table"],
+      "on"
+    );
 
-  loader("f");
-  loader("s");
-  loader("d");
-  loader("a");
-  loader("m");
-  loader("i");
+    // GET DATA:
+    const year: number = new Date().getFullYear(),
+      month: number = new Date().getMonth() + 1,
+      day: number = new Date().getDate() - 1,
+      endpoint: string = `https://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=${method}`;
 
-  loader("month-table");
+    // Interface Declarations:
+    interface Gregorian {
+      date: string;
+      day: string;
+      year: string;
+      format: string;
+      weekday: { en: string; ar?: string };
+      month: { en: string; ar?: string; number: number };
+      designation: { abbreviated: string; expanded: string };
+    }
 
-  async function callAPI() {
-    try {
-      // GET DATA:
-      const year: number = new Date().getFullYear(),
-        month: number = new Date().getMonth() + 1,
-        day: number = new Date().getDate() - 1,
-        endpoint: string = `https://api.aladhan.com/v11/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=${method}`;
+    interface Hijri extends Gregorian {
+      holidays?: [];
+    }
 
-      // Interface Declarations:
-      interface Gregorian {
-        date: string;
-        day: string;
-        year: string;
-        format: string;
-        weekday: { en: string; ar?: string };
-        month: { en: string; ar?: string; number: number };
-        designation: { abbreviated: string; expanded: string };
-      }
+    interface Date {
+      gregorian: Gregorian;
+      hijri: Hijri;
+      readable: string;
+      timestamp: string;
+    }
 
-      interface Hijri extends Gregorian {
-        holidays?: [];
-      }
+    interface Meta {
+      latitude: number;
+      latitudeAdjustmentMethod: string;
+      longitude: number;
+      method: {
+        id: number;
+        name: string;
+        params: { Fajr: number; Isha: string };
+        location: { latitude: number; longitude: number };
+      };
+      midnightMode: string;
+      offset: {
+        Imsak: number;
+        Fajr: number;
+        Sunrise: number;
+        Dhuhr: number;
+        Asr: number;
+      };
+      school: string;
+      timezone: string;
+    }
 
-      interface Date {
-        gregorian: Gregorian;
-        hijri: Hijri;
-        readable: string;
-        timestamp: string;
-      }
+    interface Timings {
+      Asr: string;
+      Dhuhr: string;
+      Fajr: string;
+      Firstthird: string;
+      Imsak: string;
+      Isha: string;
+      Lastthird: string;
+      Maghrib: string;
+      Midnight: string;
+      Sunrise: string;
+      Sunset: string;
+    }
 
-      interface Meta {
-        latitude: number;
-        latitudeAdjustmentMethod: string;
-        longitude: number;
-        method: {
-          id: number;
-          name: string;
-          params: { Fajr: number; Isha: string };
-          location: { latitude: number; longitude: number };
-        };
-        midnightMode: string;
-        offset: {
-          Imsak: number;
-          Fajr: number;
-          Sunrise: number;
-          Dhuhr: number;
-          Asr: number;
-        };
-        school: string;
-        timezone: string;
-      }
+    interface DayDataObj {
+      date: Date;
+      meta: Meta;
+      timings: Timings;
+    }
 
-      interface Timings {
-        Asr: string;
-        Dhuhr: string;
-        Fajr: string;
-        Firstthird: string;
-        Imsak: string;
-        Isha: string;
-        Lastthird: string;
-        Maghrib: string;
-        Midnight: string;
-        Sunrise: string;
-        Sunset: string;
-      }
+    // API calling:
+    const response = await axios.get(endpoint), // Todo, Type Annotation
+      res: DayDataObj[] = response.data.data;
 
-      interface DayDataObj {
-        date: Date;
-        meta: Meta;
-        timings: Timings;
-      }
+    // HANDLING WITH DATE:
+    const gregorian: Gregorian = res[day].date.gregorian,
+      gFullDateNo: string = gregorian.date,
+      gYear: string = gregorian.year,
+      gMonth: string = gregorian.month.en,
+      gDay: string = gregorian.weekday.en,
+      gDayNo: string = gregorian.day;
 
-      // API calling:
-      const response = await axios.get(endpoint), // Todo, Type Annotation
-        res: DayDataObj[] = response.data.data;
+    const hijri: Hijri = res[day].date.hijri,
+      hFullDateNo: string = hijri.date,
+      hYear: string = hijri.year,
+      hMonth: string = hijri.month.en,
+      hDay: string = hijri.weekday.en,
+      hDayNo: string = hijri.day;
 
-      // HANDLING WITH DATE:
-      const gregorian: Gregorian = res[day].date.gregorian,
-        gFullDateNo: string = gregorian.date,
-        gYear: string = gregorian.year,
-        gMonth: string = gregorian.month.en,
-        gDay: string = gregorian.weekday.en,
-        gDayNo: string = gregorian.day;
-
-      const hijri: Hijri = res[day].date.hijri,
-        hFullDateNo: string = hijri.date,
-        hYear: string = hijri.year,
-        hMonth: string = hijri.month.en,
-        hDay: string = hijri.weekday.en,
-        hDayNo: string = hijri.day;
-
-      (document.getElementById("Gregorian") as HTMLDivElement).innerHTML = `
+    (document.getElementById("Gregorian") as HTMLDivElement).innerHTML = `
       <h2 class="title-font">Gregorian</h2>
       <h5 class="secondary-font mt-4">${gFullDateNo}</h5>
       <h5 class="secondary-font mt-1">${gDay}  ${gDayNo} ${gMonth}  ${gYear}</h5>
       `;
 
-      (document.getElementById("Hijri") as HTMLDivElement).innerHTML = `
+    (document.getElementById("Hijri") as HTMLDivElement).innerHTML = `
       <h2 class="title-font">Hijri</h2>
       <h5 class="secondary-font mt-4">${hFullDateNo}</h5>
       <h5 class="secondary-font mt-1">${hDay}  ${hDayNo} ${hMonth}  ${hYear}</h5>
       `;
 
-      // HANDLING WITH TODAY'S TIMINGS:
-      const timings: Timings = res[day].timings,
-        fajr: string = to12Format(timings.Fajr.slice(0, 5)),
-        sunrise: string = to12Format(timings.Sunrise.slice(0, 5)),
-        dhuhr: string = to12Format(timings.Dhuhr.slice(0, 5)),
-        asr: string = to12Format(timings.Asr.slice(0, 5)),
-        maghrib: string = to12Format(timings.Maghrib.slice(0, 5)),
-        isha: string = to12Format(timings.Isha.slice(0, 5));
+    // HANDLING WITH TODAY'S TIMINGS:
+    const timings: Timings = res[day].timings,
+      fajr: string = to12Format(timings.Fajr.slice(0, 5)),
+      sunrise: string = to12Format(timings.Sunrise.slice(0, 5)),
+      dhuhr: string = to12Format(timings.Dhuhr.slice(0, 5)),
+      asr: string = to12Format(timings.Asr.slice(0, 5)),
+      maghrib: string = to12Format(timings.Maghrib.slice(0, 5)),
+      isha: string = to12Format(timings.Isha.slice(0, 5));
 
-      (document.getElementById("f") as HTMLDivElement).innerHTML = `
+    (document.getElementById("f") as HTMLDivElement).innerHTML = `
       <h4 class="secondary-font mb-0">${fajr}</h4>
       `;
-      (document.getElementById("s") as HTMLDivElement).innerHTML = `
+    (document.getElementById("s") as HTMLDivElement).innerHTML = `
       <h4 class="secondary-font mb-0">${sunrise}</h4>
       `;
-      (document.getElementById("d") as HTMLDivElement).innerHTML = `
+    (document.getElementById("d") as HTMLDivElement).innerHTML = `
       <h4 class="secondary-font mb-0">${dhuhr}</h4>
       `;
-      (document.getElementById("a") as HTMLDivElement).innerHTML = `
+    (document.getElementById("a") as HTMLDivElement).innerHTML = `
       <h4 class="secondary-font mb-0">${asr}</h4>
       `;
-      (document.getElementById("m") as HTMLDivElement).innerHTML = `
+    (document.getElementById("m") as HTMLDivElement).innerHTML = `
       <h4 class="secondary-font mb-0">${maghrib}</h4>
       `;
-      (document.getElementById("i") as HTMLDivElement).innerHTML = `
+    (document.getElementById("i") as HTMLDivElement).innerHTML = `
       <h4 class="secondary-font mb-0">${isha}</h4>
       `;
 
-      // HANDLING WITH DAYS OF MONTH TIMINGS:
-      (document.getElementById("month-table") as HTMLTableElement).innerHTML = `
+    // HANDLING WITH DAYS OF MONTH TIMINGS:
+    (document.getElementById("month-table") as HTMLTableElement).innerHTML = `
         <thead class="align">
             <tr>
                 <th scope="col"></th>
@@ -215,40 +221,43 @@ function getDataFromAPI(
         </thead>
         <tbody id="tbody" class="align"> </tbody>
       `;
-      const tableBody = document.getElementById(
-        "tbody"
-      ) as HTMLTableSectionElement;
-      tableBody.innerHTML = "";
+    const tableBody = document.getElementById(
+      "tbody"
+    ) as HTMLTableSectionElement;
+    tableBody.innerHTML = "";
 
-      res.forEach((obj: DayDataObj) => {
-        const Day = obj.date.gregorian.weekday.en.slice(0, 3),
-          date = obj.date.readable.slice(0, 6),
-          Fajr = to12Format(obj.timings.Fajr.slice(0, 5)),
-          Sunrise = to12Format(obj.timings.Sunrise.slice(0, 5)),
-          Dhuhr = to12Format(obj.timings.Dhuhr.slice(0, 5)),
-          Asr = to12Format(obj.timings.Asr.slice(0, 5)),
-          Maghrib = to12Format(obj.timings.Maghrib.slice(0, 5)),
-          Isha = to12Format(obj.timings.Isha.slice(0, 5));
+    res.forEach((obj: DayDataObj) => {
+      const Day = obj.date.gregorian.weekday.en.slice(0, 3),
+        date = obj.date.readable.slice(0, 6),
+        Fajr = to12Format(obj.timings.Fajr.slice(0, 5)),
+        Sunrise = to12Format(obj.timings.Sunrise.slice(0, 5)),
+        Dhuhr = to12Format(obj.timings.Dhuhr.slice(0, 5)),
+        Asr = to12Format(obj.timings.Asr.slice(0, 5)),
+        Maghrib = to12Format(obj.timings.Maghrib.slice(0, 5)),
+        Isha = to12Format(obj.timings.Isha.slice(0, 5));
 
-        const todayMark =
-          date.match(/\d+/)![0] === `${new Date().getDate()}`
-            ? "today-mark"
-            : "";
+      const todayMark =
+        date.match(/\d+/)![0] === `${new Date().getDate()}` ? "today-mark" : "";
 
-        if (screen.width >= 768) {
-          tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
-        } else {
-          tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} <br/> ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
-        }
-      });
-    } catch (err) {
-      // Todo, 'err' is of type 'unknown'
-      alert(err.message);
-      throw new Error(`${err.code},\n${err.message}`);
-    }
+      if (screen.width >= 768) {
+        tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
+      } else {
+        tableBody.innerHTML += `<tr class="${todayMark}"> <th scope="row">${Day} <br/> ${date}</th> <td>${Fajr}</td> <td>${Sunrise}</td> <td>${Dhuhr}</td> <td>${Asr}</td> <td>${Maghrib}</td> <td>${Isha}</td> </tr>`;
+      }
+    });
+  } catch (err: any) {
+    // Todo, Type Annotation
+    alert(err.message);
+
+    // STOP THE LOADER:
+    loader(
+      ["Hijri", "Gregorian", "f", "s", "d", "a", "m", "i", "month-table"],
+      "off",
+      err.message
+    );
+
+    throw new Error(`${err.code},\n${err.message}`);
   }
-
-  callAPI();
 }
 
 // Function to convert the time from 24 to 12 format:
